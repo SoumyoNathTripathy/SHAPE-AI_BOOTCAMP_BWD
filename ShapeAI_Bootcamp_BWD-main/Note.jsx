1c8b67c0bc8e839eced502e93df5906fab28620e
SoumyoNathTripathy <85414445+SoumyoNathTripathy@users.noreply.github.com>
2021-06-06 01:35:09 +0530

    Add files via upload

---
+++ ShapeAI_Bootcamp_BWD-main/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className="note"> 
+      <h1>JavaScript and React.js</h1>
+      <p> This bootcamp is taken by Shaurya Sinha. We covered the basics from Scratch 
+        including Javascript and react.js ,HTML</p>
+    </div>
+
+   
+  );
+}
+
+export default Note;
---
+++ ShapeAI_Bootcamp_BWD-main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+ReactDom.render(
+<div>
+ 
+   <App />
+
+</div> , 
+document.getElementById("root")
+) 
+
